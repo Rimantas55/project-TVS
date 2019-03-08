a7203ca55886b5e96e8bb755d4ea514164ce14d0
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-03-08 22:44:52 +0200

    Neveikia ikelimas info i DB
    Spaudziam submit, turetu ikelti i info i DB, pachekinti connection, pasirinkti viska kas yra DB ir output i narsykle. taciau kazkodel neveikia.

--- process.php
+++ process.php
@@ -6,16 +6,20 @@ if(isset($_POST["submit"])){
 
 }
 
+
 //prijungem mysqli connection is kito file t.y db.php
 include "db.php";
+
 
 //inserting info from html form to Mysql database 
 $query = "INSERT INTO users(username, password) ";
 $query .= "VALUES ('$username', '$password')";
 
+
 // * zvaigzdute reiskia ALL
-// query for reading information from database and output can be with while loop
+// query for reading information from database and output can be done with while loop
 $query = "SELECT * FROM users";
+
 
 //Checking if info insertings working good
 $result = mysqli_query($connection, $query);
@@ -25,7 +29,7 @@ if(!$result) {
 	die('Query FAILED' . mysqli_error($connection));
 }
 
-//method how to check what kind info have database / output info from database
+//method how to check what kind info we have in database / output info from database
 while($row = mysqli_fetch_assoc($result)) {
 	echo "<pre>";
 	print_r($row);
